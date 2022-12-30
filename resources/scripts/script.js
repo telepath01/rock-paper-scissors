fa070c6a34a32a4c343fc33f911f5f4d60eab847
telepath01 <47895030+telepath01@users.noreply.github.com>
2022-12-30 15:09:02 -0500

    Game Complete
    Finished all sections. Game goes through the code and tells player who won. allows player to reset game and start over.

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -2,12 +2,17 @@
 const buttons = document.querySelectorAll('button');
 const playerScoreValue = document.querySelector('.player-score-value');
 const cpuScoreValue = document.querySelector('.cpu-score-value');
+const winScreen = document.getElementById('win-screen');
+const winMsg = document.querySelector('.win-header');
+const winScore = document.querySelector('.win-score');
+const restartbtn = document.querySelector('.restart-btn');
 
 const playerWinMsg = 'Player wins the game! ';
 const playerLoseMsg = 'Player lost the game! ';
 let messege = '';
 let playerScore = 0;
 let computerScore = 0;
+let win = false;
 
 function getComputerChoice() {
   const startValues = ['rock', 'paper', 'scissors'];
@@ -18,12 +23,35 @@ function getComputerChoice() {
 function updateScore(messege) {
   if (messege.includes(`${playerWinMsg}`)) {
     playerScore++;
-    console.log(playerScore);
   } else if (messege.includes(`${playerLoseMsg}`)) {
     computerScore++;
   }
   cpuScoreValue.textContent = computerScore;
   playerScoreValue.textContent = playerScore;
+  winCheck(playerScore, computerScore);
+}
+function winCheck(pScore, cScore) {
+  if (pScore === 5 || cScore === 5) {
+    winScreen.style.display = 'grid';
+    if (pScore >= 5) {
+      winMsg.textContent = 'Player Wins !!!';
+      winScore.textContent = `${playerScore}`;
+      win = true;
+    } else if (cScore >= 5) {
+      win = true;
+      winMsg.textContent = 'Computer Wins !!!';
+      winScore.textContent = `${computerScore}`;
+    }
+  }
+}
+
+function gameRestart() {
+  playerScore = 0;
+  computerScore = 0;
+  winScreen.style.display = 'none';
+  win = false;
+  cpuScoreValue.textContent = '0';
+  playerScoreValue.textContent = '0';
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -60,19 +88,27 @@ function playRound(playerSelection, computerSelection) {
 
 buttons.forEach(button =>
   button.addEventListener('click', function (e) {
-    if (e.target.classList.value === 'rock-btn') {
-      const playerChoice = 'rock';
-      // console.log('works');
-      playRound(playerChoice, getComputerChoice());
-    } else if (e.target.classList.value === 'paper-btn') {
-      const playerChoice = 'paper';
-      // console.log('paper');
-      playRound(playerChoice, getComputerChoice());
-    } else if (e.target.classList.value === 'scissors-btn') {
-      const playerChoice = 'scissors';
-      // console.log(playerChoice);
-      playRound(playerChoice, getComputerChoice());
+    if (!win) {
+      if (e.target.classList.value === 'rock-btn') {
+        const playerChoice = 'rock';
+        // console.log('works');
+        playRound(playerChoice, getComputerChoice());
+      } else if (e.target.classList.value === 'paper-btn') {
+        const playerChoice = 'paper';
+        // console.log('paper');
+        playRound(playerChoice, getComputerChoice());
+      } else if (e.target.classList.value === 'scissors-btn') {
+        const playerChoice = 'scissors';
+        // console.log(playerChoice);
+        playRound(playerChoice, getComputerChoice());
+      }
+    } else {
     }
+
     updateScore(messege);
   })
 );
+
+restartbtn.addEventListener('click', function () {
+  gameRestart();
+});
